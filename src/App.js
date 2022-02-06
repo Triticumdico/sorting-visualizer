ac3d779af04ae8d207798f8156b776c9c970f096
Nicolas Weber <nicopaul.weber@gmail.com>
2022-02-06 11:44:22 +0100

    create merge sort algorithm

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
+import React from 'react';
 import './App.css';
+import SortingVisualizer from './Components/SortingVisualizer';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      
+      <SortingVisualizer> </SortingVisualizer>
     </div>
   );
 }
-
-export default App;
---
+++ src/Components/SortingVisualizer.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { mergeSort } from "./SortingAlgoritms";
+import './SortingVisualizer.css'
+
+export default class SortingVisualizer extends React.Component {
+    constructor(props) { // create constructor
+        super(props); // call parent constructor with props parameter 
+        this.state = {array: []}; 
+    } 
+
+    // when component load for the first time call reset Array to inits
+    componentDidMount() {
+        this.resetArray();
+    } 
+    
+    resetArray() {
+        const array = [];
+        for (let i = 0; i < 300; i++) {
+            array.push(randomIntFromInterval(5, 500));
+        }
+        this.setState({array}); // set state with the array 
+    }
+
+    mergeSort() {
+        const javaScriptSortedArray = this.state.array.slice().sort((a, b) => a - b);
+        const sortedArray = mergeSort(this.state.array);
+
+        console.log(sortedArray)
+        console.log(arraysAreEquals(javaScriptSortedArray, sortedArray));
+    } 
+
+
+    render() {
+        const {array} = this.state;
+
+        return (
+          <div className="array-container">
+              {array.map((value, idx) => (
+                <div 
+                    className="array-bar" 
+                    key={idx}
+                    style={{height : `${value}px`}}></div>
+              ))}
+              <button onClick={() => this.resetArray()}>Generate new array</button>
+              <button onClick={() => this.mergeSort()}>Merge Sort</button>              
+          </div>
+        );
+    }
+}
+
+function randomIntFromInterval(min, max) {
+    // min max included
+    // floor avoid duplicate value
+    return Math.floor(Math.random() * (max - min + 1) + 1);
+}
+
+
+function arraysAreEquals(arrayOne, arrayTwo){
+    if (arrayOne.length !== arrayTwo.length) return false;
+    for (let i = 0; i < arrayOne.length; i++) {
+        if (arrayOne[i] !== arrayTwo[i]) return false;
+    } 
+    return true;
+} 
